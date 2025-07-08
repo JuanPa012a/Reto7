920f7fcc07d51de8dc0d3ad656624f62fa10ce17
JuanPa012a <1034776660juan@gmail.com>
2025-07-08 07:31:38 -0500

    documentacion

--- reto7/src/main/java/com/devsenior/pablo/reto7/controller/AuthenticationController.java
+++ reto7/src/main/java/com/devsenior/pablo/reto7/controller/AuthenticationController.java
@@ -11,10 +11,12 @@ import com.devsenior.pablo.reto7.model.dto.AuthenticationRequest;
 import com.devsenior.pablo.reto7.model.dto.AuthenticationResponse;
 import com.devsenior.pablo.reto7.service.impl.AuthenticationServiceImpl;
 
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.tags.Tag;
 import jakarta.validation.Valid;
 import lombok.AllArgsConstructor;
     
-
+@Tag(name="Autenticacion", description="Endpoints para auntenticarse y registrarse en el sistema")
 @RestController
 @AllArgsConstructor
 public class AuthenticationController {
@@ -23,12 +25,12 @@ public class AuthenticationController {
     
 
 
-
+    @Operation(summary="Auntenticacion", description="Aca es donde nos vamos a autenticar en el sistema y recibir nuestras credenciales JWT")
     @PostMapping("/authenticate")
     public AuthenticationResponse login(@Valid @RequestBody AuthenticationRequest request) throws Exception {
         return service.login(request);
     }
-
+    @Operation(summary="Register", description="Podemos crear nuestro propio usuario para poder entrar al sistema")
     @PostMapping("/register")
     public ResponseEntity<HttpStatus> register(@Valid @RequestBody AuthenticationRequest entity, @RequestParam("rol") String rol) {
         service.saveUser(entity, rol);
--- reto7/src/main/java/com/devsenior/pablo/reto7/controller/CourseController.java
+++ reto7/src/main/java/com/devsenior/pablo/reto7/controller/CourseController.java
@@ -17,10 +17,12 @@ import org.springframework.web.bind.annotation.RestController;
 import com.devsenior.pablo.reto7.model.dto.CourseDto;
 import com.devsenior.pablo.reto7.service.ServiceCourse;
 
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.tags.Tag;
 import jakarta.validation.Valid;
 
 
-
+@Tag(name="Cursos", description="Endpoints de la gestion de cursos")
 @RestController
 @RequestMapping("/api/course")
 public class CourseController {
@@ -30,17 +32,22 @@ public class CourseController {
         this.service = service;
     }
     
+    @Operation(summary="Listar los cursos", description="Listar todos los cursos que existan en el sistema.")
     @GetMapping
     public List<CourseDto> getAllCourses() {
         return service.getAll();
     }
-    
+
+    @Operation(summary="Obtener curso por Id", description="Se obtiene un curso por su Id. "+
+    "Esta operacion solo la puede realizar el rol administardor o usuario")
     @PreAuthorize("hasRole('ADMIN') or (hasRole('USER') and #id > 0) ")
     @GetMapping("{id}")
     public CourseDto getCourseById(@PathVariable Long id) {
         return service.getById(id);
     }
     
+    @Operation(summary="Crear curso", description="Endpoint para crear un curso. "+
+    "Esta operacion solo la puede realizar el rol administardor")
     @PreAuthorize("hasRole('ADMIN')")
     @ResponseStatus(HttpStatus.CREATED)
     @PostMapping()
@@ -48,12 +55,16 @@ public class CourseController {
         return service.add(course);
     }
     
+    @Operation(summary="Eliminar curso", description="Endpoint para eliminar un curso. "+
+    "Esta operacion solo la puede realizar el rol administardor")
     @PreAuthorize("hasRole('ADMIN')")
     @DeleteMapping("{id}")
     public CourseDto deleteCourseById(@PathVariable Long id) {
         return service.delete(id);
     }
 
+    @Operation(summary="Actualizar curso", description="Endpoint para actualizar un curso. "+
+    "Esta operacion solo la puede realizar el rol administardor")
     @PreAuthorize("hasRole('ADMIN')")
     @PutMapping("{id}")
     public CourseDto updateCourse(@Valid @RequestBody CourseDto course, @PathVariable Long id) {
